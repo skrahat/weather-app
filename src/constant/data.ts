ed0e5e031dd9f61916610cc67315f43c9e0970d2
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-09-28 13:00:30 -0400

    feat: added const to store fixed variables

---
+++ src/constant/data.ts
@@ -0,0 +1,16 @@
+export const canadianCities = [
+  { name: 'Toronto', lat: 43.7, lng: -79.42 },
+  { name: 'Vancouver', lat: 49.28, lng: -123.12 },
+  { name: 'Montreal', lat: 45.51, lng: -73.59 },
+  { name: 'Calgary', lat: 51.05, lng: -114.07 },
+  { name: 'Ottawa', lat: 45.42, lng: -75.69 },
+  { name: 'Edmonton', lat: 53.54, lng: -113.49 },
+  { name: 'Quebec City', lat: 46.81, lng: -71.21 },
+  { name: 'Winnipeg', lat: 49.9, lng: -97.13 },
+  { name: 'Halifax', lat: 44.65, lng: -63.57 },
+  { name: 'Victoria', lat: 48.43, lng: -123.37 },
+];
+export const center = {
+  lat: 45.7558,
+  lng: -97.1384,
+};
